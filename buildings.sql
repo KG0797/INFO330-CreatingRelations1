7adcd6978d2dcb9a358db8dd881226cee6256337
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-12 10:56:08 -0700

    Create buildings.sql

---
+++ buildings.sql
@@ -0,0 +1,2 @@
+CREATE TABLE buildings(name VACHAR(80) NOT NULL, shortname VACHAR(10) UNIQUE, id INTEGER PRIMARY KEY);
+CREATE TABLE rooms(number INTEGER, buildingid INTEGER PRIMARY KEY, seating INTEGER);
