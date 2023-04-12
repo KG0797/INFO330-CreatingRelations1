97ec15a7b7a089c04707fe080323f8fc95e08d52
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-12 10:00:05 -0700

    Update students.sql

--- students.sql
+++ students.sql
@@ -1,1 +1,12 @@
 CREATE TABLE students(ID INTEGER PRIMARY KEY, first_name VACHAR(40), last_name VACHAR(80), age NUMERIC);
+INSERT INTO students(ID, first_name, last_name, age) 
+VALUES (1, 'Fred', 'Flintstone', 35),
+    (2, 'Wilma', 'Flintstone', 29),
+    (3, 'Barney', 'Rubble', 33),
+    (4, 'Betty', 'Rubble', 29),
+    (5, 'Pebbles', 'Flintstone', 1),
+    (6, 'Bam-Bam', 'Rubble', 1),
+    (7, 'Mary', 'Wang', 25),
+    (8, 'Bob', 'Johnson', 42),
+    (9, 'Katherine', 'Guo', 31);
+ 
