0dad8594b3636fb6a1db65ca3a82d0c50a2895ff
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-13 09:21:09 -0700

    Update buildings.sql

--- buildings.sql
+++ buildings.sql
@@ -1,2 +1,2 @@
 CREATE TABLE buildings(name VACHAR(80) NOT NULL, shortname VACHAR(10) UNIQUE, id INTEGER PRIMARY KEY);
-CREATE TABLE rooms(number INTEGER, buildingid INTEGER PRIMARY KEY, seating INTEGER);
+CREATE TABLE rooms(number INTEGER, buildingid INTEGER FOREIGN KEY(buildingid) REFERENCES building(id), seating INTEGER);
