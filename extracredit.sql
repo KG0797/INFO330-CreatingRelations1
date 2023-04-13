1c2169d1a05f84b9a6a7c9b6df4c94bacb44a626
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-13 09:14:47 -0700

    Update extracredit.sql

--- extracredit.sql
+++ extracredit.sql
@@ -1,3 +1,10 @@
+CREATE TEMPORARY TABLE temp_rooms(id INTEGER PRIMARY KEY, number INTEGER, buildingid INTEGER PRIMARY KEY, seating INTEGER);
+
+INSERT INTO temp_rooms (number, buildingid, seating)
+SELECT number, buildingid, seating FROM rooms;
+
+DELETE TABLE rooms;
+ALTER TABLE temp_rooms RENAME TO rooms;
 ALTER TABLE rooms ADD COLUMN id INTEGER PRIMARY KEY;
 
 CREATE TABLE timeslots(id INTEGER PRIMARY KEY, start_time TIMESTAMP NOT NULL, end_time TIMESTAMP NOT NULL);
