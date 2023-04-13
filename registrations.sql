b2d93ffba178d8e4b2d4ce03539a177e35fca55b
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-13 08:41:36 -0700

    Update registrations.sql

--- registrations.sql
+++ registrations.sql
@@ -2,5 +2,5 @@ CREATE TABLE registrations(studentid INTEGER FOREIGN KEY(studentid）REFERENCEs 
 INSERT INTO registrations（studentid, course, grade)
 VALUES ('1', 'INFO330A', NULL), ('1', 'INFO448A', NULL), ('1', 'INFO314', NULL),
 ('3', 'INFO330A', NULL), ('3', 'INF0449A', NULL),
-('2', 'BAW010', NULL), ('2', 'BAW100', NULL),
-('4', 'BAW010', NULL);
+('2', 'BAW0100', NULL), ('2', 'BAW100A', NULL),
+('4', 'BAW0100', NULL);
