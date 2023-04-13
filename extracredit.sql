8cbbe651b2bde537e06cc82282f35aef45d22652
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-12 23:28:57 -0700

    Update extracredit.sql

--- extracredit.sql
+++ extracredit.sql
@@ -1,4 +1,6 @@
+ALTER TABLE rooms ADD COLUMN id INTEGER PRIMARY KEY;
+
 CREATE TABLE timeslots(id INTEGER PRIMARY KEY, start_time TIMESTAMP NOT NULL, end_time TIMESTAMP NOT NULL);
 CREATE TABLE schedule(coursecode, roomid INTEGER, timeslotid INTEGER UNIQUE,  
-PRIMARY KEY(roomid, timeslotid), FOREIGN KEY (coursecode) REFERENCES course(CODE),
+PRIMARY KEY(roomid, timeslotid), FOREIGN KEY (coursecode) REFERENCES courses(CODE),
 FOREIGN KEY(roomid) REFERENCES rooms(id), FOREIGN KEY(timeslotid) REFERENCES timeslots(id));
